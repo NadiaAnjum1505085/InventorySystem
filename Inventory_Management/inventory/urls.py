68a2ca7a8a1a83e6906186a7490dec89fadfc5e7
nadiaanjum <nadia4anjum421@gmail.com>
2021-06-24 23:03:29 +0600

    url pattern thik korsi

--- Inventory_Management/inventory/urls.py
+++ Inventory_Management/inventory/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name='Inventory'
 urlpatterns = [
     path('',views.index,name='index'),
+    path('register/',views.register,name='register'),
 ]
--- Inventory_Management/inventory/views.py
+++ Inventory_Management/inventory/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>INVENTORY MANAGEMENT SYSTEM</h1>')
+    #return HttpResponse('<h1>INVENTORY MANAGEMENT SYSTEM</h1>')
+    return render(request,'Inventory/index.html')
+def register(request):
+    #return HttpResponse('<h1>INVENTORY MANAGEMENT SYSTEM</h1>')
+    return render(request,'Inventory/register.html')
