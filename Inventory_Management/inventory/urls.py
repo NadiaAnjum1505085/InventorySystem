949627694763ed2a1dd6acc84c7aa6b3a7c85c79
nadiaanjum <nadia4anjum421@gmail.com>
2021-06-29 23:45:50 +0600

    Get token

--- Inventory_Management/inventory/urls.py
+++ Inventory_Management/inventory/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from . import views
+#from . import views
+from rest_framework.authtoken import views
 
 app_name='Inventory'
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('register/',views.register,name='register'),
-    path('login/',views.login,name='login'),
-    path('token/',views.TokenCreate,name='token')
+    # path('',views.index,name='index'),
+    # path('register/',views.register,name='register'),
+    # path('login/',views.login,name='login'),
+    # path('token/',views.TokenCreate,name='token'),
+    path('api-token-auth/', views.obtain_auth_token),
 ]
